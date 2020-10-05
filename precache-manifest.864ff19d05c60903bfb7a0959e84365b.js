1ceca30a42e86f83225f031cb71aec1dfea34eb8
devs <doc1@gmail.com>
2020-10-05 18:54:20 +1100

    launch

---
+++ precache-manifest.864ff19d05c60903bfb7a0959e84365b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "991030ffa606f947df54",
+    "url": "/static/css/main.44fdb6e8.chunk.css"
+  },
+  {
+    "revision": "991030ffa606f947df54",
+    "url": "/static/js/main.991030ff.chunk.js"
+  },
+  {
+    "revision": "bb7d72a223142cf1e22e",
+    "url": "/static/js/1.bb7d72a2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "62974dd584f3ef8d12f4ffc6a2748579",
+    "url": "/index.html"
+  }
+];
